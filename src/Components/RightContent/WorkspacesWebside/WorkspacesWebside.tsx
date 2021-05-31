f9b10f766126fde8ef1c956b08d00e2719d60c76
My Name <66422260+Radek25@users.noreply.github.com>
2021-05-31 16:49:00 +0200

    First version of app before refactoring

--- src/Components/RightContent/WorkspacesWebside/WorkspacesWebside.tsx
+++ src/Components/RightContent/WorkspacesWebside/WorkspacesWebside.tsx
@@ -2,24 +2,53 @@ import React, {FC, useState} from 'react';
 import {useParams} from "react-router-dom";
 
 //Styled Import
-import { WorkspacesWrapper } from './../HomeWebside/Workspaces/WorkspacesStyled';
+import { WorkspacesWebsideWrapper } from './WorkspacesWebsideStyled';
 
 import { WorkspacesArray } from './../HomeWebside/Workspaces/Slider/Slider';
+
+import { ResumeYourWork } from './../HomeWebside/ResumeYourWork/ResumeYourWork';
+
+//Icons Import
+import Building from '../../../icons/building-workspaces.png';
+import Connection from '../../../icons/sitemap-workspaces.png';
+import Calendar from '../../../icons/calendar-workspaces.png';
 
 export const WorkspacesWebside: FC = () => {
 
     const {id} = useParams<{id: string}>();
 
     const Workspaces = WorkspacesArray.find(Workspaces => Workspaces.Id == parseInt(id))
- 
+  
     return(
-        <WorkspacesWrapper>
-            <header>
-                <div className='title-and-desc'>
-
+       <WorkspacesWebsideWrapper photo={Workspaces?.BackgroundImg}>
+           <div className='title-and-photo'>
+               <div className='bottom-module'>
+                    <img src={Workspaces?.MainIcon}/>
+                    <div className='title-of-workspaces'>
+                        <h1>{Workspaces?.TitleOfTile}</h1>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae, sapiente. Consectetur quasi praesentium laborum cum dolores est dolorum, perferendis, vero repudiandae, tempore totam. Ad accusamus laborum sint. Aliquid, id alias.</p>
+                    </div>
                 </div>
-            </header>
-            {Workspaces?.TitleOfTile}
-        </WorkspacesWrapper>
+           </div>
+           <div className='start-working'>
+               <p>Start working on corporate matters</p>
+               <div className='tiles'>
+                   <img src={Building}/>
+                   <h2>Explore yor entities</h2>
+                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae error repellat incidunt.</p>
+               </div>
+               <div className='tiles'>
+                    <img src={Connection}/>
+                    <h2>Structure the ownership</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam numquam id accusantium error saepe.</p>
+               </div>
+               <div className='tiles'>
+                    <img src={Calendar}/>
+                    <h2>Define the calendar</h2>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. </p>
+               </div>
+           </div>
+           <ResumeYourWork/>
+       </WorkspacesWebsideWrapper> 
     );
 };
--- src/Components/RightContent/WorkspacesWebside/WorkspacesWebsideStyled.tsx
+++ src/Components/RightContent/WorkspacesWebside/WorkspacesWebsideStyled.tsx
@@ -1,15 +1,64 @@
 import styled from 'styled-components';
-import {Colors} from '../../../StyledHelpers/Colors';
+import { Colors } from './../../../StyledHelpers/Colors';
 
-interface WorkspacesData{
-    a: any;
+interface WorkspacesWebsideData{
+    photo: string | undefined;
 }
 
-export const WorkspacesWrapper = styled.div<WorkspacesData>`
+export const WorkspacesWebsideWrapper = styled.div<WorkspacesWebsideData>`
     width: 95%;
-    background-color: white;
-    
-    header{
+    font-family: 'Roboto', sans-serif;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    .title-and-photo{
         width: 90%;
+        height: 250px;
+        margin-bottom: 30px;
+        display: flex;
+        align-items: flex-end;
+        border: 2px solid ${Colors.lightgrey};
+        border-radius: 10px;
+        background-image: url(${props => props.photo});
+        background-position: center;
+        background-size: cover;
+        .bottom-module{
+            display: flex;
+            align-items: center;
+            padding: 5px 20px;
+            background-color: white;
+            border-top: 2px solid ${Colors.lightgrey};
+            border-radius: 0 0 10px 10px;
+        }
+        .title-of-workspaces{
+            margin: 0 20px;
+        }
+    }
+    .start-working{
+        width: 95%;
+        background-color: ${Colors.lightgrey};
+        padding: 20px 10px;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        border-radius: 10px;
+        margin-bottom: 30px;
+        .tiles{
+            width: 28%;
+            height: 200px;
+            margin: 20px 10px 20px;
+            display: flex;
+            flex-wrap: wrap;
+            background-color: ${Colors.white};
+            border-radius: 10px;
+            padding: 10px;
+            img{
+                width: 50px;
+                height: 50px;
+            }
+        }
+        p{
+            width: 100%;
+        }
     }
 `;
